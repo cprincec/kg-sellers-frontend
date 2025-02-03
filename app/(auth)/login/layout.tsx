3b5092692cf32db4960c049e094ec46a3bb58d6a
cprincec <chukwuprincec@gmail.com>
2025-02-03 11:05:41 +0100

    wrapped login in suspense tags

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,7 +1,12 @@
 import { OtpContextProvider } from "@/contexts/otpContext";
+import { Suspense } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return <OtpContextProvider>{children}</OtpContextProvider>;
+    return (
+        <OtpContextProvider>
+            <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        </OtpContextProvider>
+    );
 };
 
 export default Layout;
