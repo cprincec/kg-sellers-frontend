959d2013ed018457bf88289735834da277876b08
cprincec <chukwuprincec@gmail.com>
2025-02-11 14:53:08 +0100

    added error boundary to dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from "react";
 import SideBarDesktop from "./ui/navigation/SideBarDesktop";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="md:grid grid-flow-col md:bg-kaiglo_grey-100">
-            {/* Navigation Bar */}
-            <SideBarDesktop />
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="md:grid grid-flow-col md:bg-kaiglo_grey-100">
+                {/* Navigation Bar */}
+                <SideBarDesktop />
 
-            <div className="md:w-[65%] lg:w-[80%] ml-auto">{children}</div>
-        </div>
+                <div className="md:w-[65%] lg:w-[80%] ml-auto">{children}</div>
+            </div>
+        </Suspense>
     );
 };
 
