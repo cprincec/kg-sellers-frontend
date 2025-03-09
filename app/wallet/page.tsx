918502d3a89d392c0e718a4063833bab9f0a32e1
cprincec <chukwuprincec@gmail.com>
2025-03-09 02:47:31 +0100

    added layout to wallet

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -8,7 +8,6 @@ import WalletHistoryTableWrapper from "./ui/walletTable/WalletHistoryTableWrappe
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import PayoutHistoryTableWrapper from "./ui/payoutTable/PayoutHistoryTableWrapper";
 import OrderHistoryToolsBar from "../orders/ui/OrderHistoryToolsBar";
-import DashboardLayout from "@/components/layouts/DashboardLayout";
 
 const Wallet = () => {
     const searchParams = useSearchParams();
@@ -26,69 +25,67 @@ const Wallet = () => {
     };
 
     return (
-        <DashboardLayout>
-            <div className="pb-4 grid gap-2 md:gap-1 lg:gap-0">
-                <Header heading={"Wallet"} description={"Your current transactions activities."} />
+        <div className="pb-4 grid gap-2 md:gap-1 lg:gap-0">
+            <Header heading={"Wallet"} description={"Your current transactions activities."} />
 
-                <div className="grid px-3 md:pl-1 md:pr-1 md:max-lg:p-4 lg:px-0 md:max-lg:bg-white">
-                    <AccountSummary />
-                </div>
-
-                <div className="grid gap-4 py-4 lg:py-6">
-                    <Tabs defaultValue={activeTab || "wallet"} className="grid gap-2 lg:gap-4 px-4">
-                        <div className="grid md:grid-cols-2 lg:justify-between lg:items-center gap-2 lg:gap-4 md:border-b border-kaiglo_grey-200">
-                            <OrderHistoryToolsBar
-                                showSort={false}
-                                showAction={false}
-                                className="px-0 justify-end border-none"
-                            />
-
-                            {/* TABS */}
-                            <TabsList className="lg:order-first flex items-center justify-start pt-0 bg-transparent rounded-none">
-                                {tabs.map((tab) => (
-                                    <TabsTrigger
-                                        key={tab.label}
-                                        value={tab.value}
-                                        className="px-4 py-2"
-                                        onClick={() => setActiveTab(tab.value)}
-                                    >
-                                        {tab.label}
-                                    </TabsTrigger>
-                                ))}
-                            </TabsList>
-                        </div>
-
-                        {/* Tables */}
-                        <div className="overflow-x-auto">
-                            {/* render the wallet history table by default
-                       render the payout table based on active tab  */}
-                            {tabs.map((tab) => {
-                                if (tab.value === "payout")
-                                    return (
-                                        <TabsContent
-                                            key={`${tab.value}`}
-                                            value={tab.value}
-                                            className="overflow-auto"
-                                        >
-                                            <PayoutHistoryTableWrapper />
-                                        </TabsContent>
-                                    );
-                                else
-                                    return (
-                                        <TabsContent
-                                            key={`${tab.value}`}
-                                            value={tab.value}
-                                            className="overflow-auto"
-                                        >
-                                            <WalletHistoryTableWrapper />
-                                        </TabsContent>
-                                    );
-                            })}
-                        </div>
-                    </Tabs>
-                </div>
+            <div className="grid px-3 md:pl-1 md:pr-1 md:max-lg:p-4 lg:px-0 md:max-lg:bg-white">
+                <AccountSummary />
             </div>
-        </DashboardLayout>
+
+            <div className="grid gap-4 py-4 lg:py-6">
+                <Tabs defaultValue={activeTab || "wallet"} className="grid gap-2 lg:gap-4 px-4">
+                    <div className="grid md:grid-cols-2 lg:justify-between lg:items-center gap-2 lg:gap-4 md:border-b border-kaiglo_grey-200">
+                        <OrderHistoryToolsBar
+                            showSort={false}
+                            showAction={false}
+                            className="px-0 justify-end border-none"
+                        />
+
+                        {/* TABS */}
+                        <TabsList className="lg:order-first flex items-center justify-start pt-0 bg-transparent rounded-none">
+                            {tabs.map((tab) => (
+                                <TabsTrigger
+                                    key={tab.label}
+                                    value={tab.value}
+                                    className="px-4 py-2"
+                                    onClick={() => setActiveTab(tab.value)}
+                                >
+                                    {tab.label}
+                                </TabsTrigger>
+                            ))}
+                        </TabsList>
+                    </div>
+
+                    {/* Tables */}
+                    <div className="overflow-x-auto">
+                        {/* render the wallet history table by default
+                       render the payout table based on active tab  */}
+                        {tabs.map((tab) => {
+                            if (tab.value === "payout")
+                                return (
+                                    <TabsContent
+                                        key={`${tab.value}`}
+                                        value={tab.value}
+                                        className="overflow-auto"
+                                    >
+                                        <PayoutHistoryTableWrapper />
+                                    </TabsContent>
+                                );
+                            else
+                                return (
+                                    <TabsContent
+                                        key={`${tab.value}`}
+                                        value={tab.value}
+                                        className="overflow-auto"
+                                    >
+                                        <WalletHistoryTableWrapper />
+                                    </TabsContent>
+                                );
+                        })}
+                    </div>
+                </Tabs>
+            </div>
+        </div>
     );
 };
 
