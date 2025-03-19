f6531fd39061ade4efe6303e6aa394fe418ec33d
cprincec <chukwuprincec@gmail.com>
2025-03-19 19:44:55 +0100

    bug fix

--- app/(unauthenticatedRoutes)/layout.tsx
+++ app/(unauthenticatedRoutes)/layout.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from "react";
 import LandingPageHeader from "./ui/LandingPageHeader";
 import Footer from "./ui/footer/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="max-w-[1600px] mx-auto">
-            <LandingPageHeader />
-            {children}
-            <Footer />
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="max-w-[1920px] mx-auto">
+                <LandingPageHeader />
+                {children}
+                <Footer />
+            </div>
+        </Suspense>
     );
 };
 
