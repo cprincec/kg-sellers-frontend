e386c1cc2bb5643108128610517d451aed3716a6
cprincec <chukwuprincec@gmail.com>
2025-03-19 06:34:13 +0100

    bug fix

---
+++ app/ui/StartSellingButton.tsx
@@ -0,0 +1,19 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const StartSellingButton = ({ className }: { className?: string }) => {
+    return (
+        <Link
+            href={"/register"}
+            className={cn(
+                buttonVariants({ variant: "primary" }),
+                "justify-self-center px-6 md:px-8 py-4 md:py-6 text-xl font-bold rounded-xl",
+                className
+            )}
+        >
+            Start selling
+        </Link>
+    );
+};
+export default StartSellingButton;
