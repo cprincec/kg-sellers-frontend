87364e9aa650838e43d4829f7166270cc6843c05
cprincec <chukwuprincec@gmail.com>
2025-05-14 22:00:53 +0100

    fixed bug on loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,5 @@
 import Loader from "./ui/Loader";
 
 export default function Loading() {
-    return (
-        <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
-            <Loader />
-        </div>
-    );
+    return <Loader />;
 }
