4117b85800bf830d192277f9485462904d078920
cprincec <chukwuprincec@gmail.com>
2025-07-08 09:51:52 +0100

    store setup integration complete

--- app/(auth)/ui/register/storeSetup/productsCategories/ProductsCategoriesFormWrapper.tsx
+++ app/(auth)/ui/register/storeSetup/productsCategories/ProductsCategoriesFormWrapper.tsx
@@ -9,16 +9,7 @@ const ProductsCategoriesFormWrapper = () => {
 
     if (!onboardingData?.productsCategories) return <Loader />;
 
-    const categories = onboardingData.productsCategories.category;
-    let defaultValues = onboardingData.productsCategories;
-
-    // The backend currently return duplicate categories
-    // So, filter it before displaying as options to user
-    if (categories.length) {
-        defaultValues = { category: [...new Set(categories.map((c) => c.toLowerCase()))] };
-    }
-
-    return <ProductsCategoriesForm defaultValues={defaultValues} />;
+    return <ProductsCategoriesForm defaultValues={onboardingData.productsCategories} />;
 };
 
 export default ProductsCategoriesFormWrapper;
