9a7bbefe7615436bce7c29d3fa004b3860f88bf8
cprincec <chukwuprincec@gmail.com>
2025-09-22 09:53:55 +0100

    withdraw completed

--- components/editor/editor-ui/image-component.tsx
+++ components/editor/editor-ui/image-component.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { JSX, Suspense, useCallback, useEffect, useRef, useState } from "react";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
-import { useCollaborationContext } from "@lexical/react/LexicalCollaborationContext";
+// import { useCollaborationContext } from "@lexical/react/LexicalCollaborationContext";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
@@ -135,7 +135,7 @@ export default function ImageComponent({
     const buttonRef = useRef<HTMLButtonElement | null>(null);
     const [isSelected, setSelected, clearSelection] = useLexicalNodeSelection(nodeKey);
     const [isResizing, setIsResizing] = useState<boolean>(false);
-    const { isCollabActive } = useCollaborationContext();
+    // const { isCollabActive } = useCollaborationContext();
     const [editor] = useLexicalComposerContext();
     const [selection, setSelection] = useState<BaseSelection | null>(null);
     const activeEditorRef = useRef<LexicalEditor | null>(null);
